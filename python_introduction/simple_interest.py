2cca3c8911dc76b5d88a03327ef38f693f5b42cd
Seth-Gbemumu <gracesefor@gmail.com>
2024-06-20 06:23:25 -0700

    Calculate simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,5 @@
-# Calculating simple interest
 principal = 1000
 rate = 0.05
 time = 3
-interest = pricipal * rate * time
+interest = principal * rate * time
 print(f"The simple interest is {interest}")
