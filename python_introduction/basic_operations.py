1f63700c386c288202afd308ce26c66c4217c6c5
Seth-Gbemumu <gracesefor@gmail.com>
2024-06-20 02:14:28 -0700

    Perform basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# Add number1 and number2
+sum = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum}")
+# Subtrat number2 from number1
+difference = number1 - number2
+print(f"Subtarction of {number1} and {number2} is {difference}")
+# Multiply number1 and number2
+product = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product}")
