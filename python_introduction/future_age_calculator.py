7875c10c1c074b55e1f8b76326aefae0c0b835c5
Seth-Gbemumu <gracesefor@gmail.com>
2024-06-20 22:13:43 -0700

    Calculate projected savings of a user from his monthly savings

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -3,4 +3,4 @@ current_year = 2023
 future_year = 2050
 difference_in_years = future_year - current_year
 future_age = current_age + difference_in_years
-print(f"In 2050, you will be {future_age} years old")
+print(f"In 2050, you will be {future_age} years old.")
