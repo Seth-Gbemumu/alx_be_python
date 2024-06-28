a94a4066edd3e8cebf2297eb505aca390467a374
Seth-Gbemumu <gracesefor@gmail.com>
2024-06-27 22:43:44 -0700

    Perform calculation from a user's input

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = input("Enter the first number: ")
 num2 = input("Enter the second number: ")
-operation = input("Enter an operation (+,*,-,/) ")
+operation = input("Choose the operation (+,*,-,/) ")
 match operation:
     case "+":
         print("The result is :", num1 + num2)
