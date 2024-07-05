b1a462bc270dea77ad80fcc37adb6c178f013ff7
Seth-Gbemumu <gracesefor@gmail.com>
2024-07-04 21:29:58 -0700

    Shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter the item you want to add to the shopping list: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} successfully added to the shopping list.")
         elif choice == '2':
