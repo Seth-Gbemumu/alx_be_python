115dab104859586da0b929278e357e98a457a334
Seth-Gbemumu <gracesefor@gmail.com>
2024-07-06 01:47:20 -0700

    Display date and time to users

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,12 +1,12 @@
-import datetime
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    from datetime import datetime
+    current_date = datetime.now()
     current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(current_date)
 
 display_current_datetime()
-#import datetime
-def calculate_future_date(): 
+import datetime 
+def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
    # future_date = datetime.timedelta(number_of_days = number_of_days)
     future_date = datetime.datetime.now() + datetime.timedelta(days = number_of_days)
