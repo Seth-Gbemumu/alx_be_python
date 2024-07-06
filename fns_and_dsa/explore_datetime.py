b36956bc3745dbdf6cb9d53177055051586fd362
Seth-Gbemumu <gracesefor@gmail.com>
2024-07-06 01:59:00 -0700

    Display date and time to users

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,7 @@
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    from datetime import datetime
+    current_date = datetime.now()
     current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(current_date)
 
