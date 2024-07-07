c8999a4fe68dd260476feadd00e63a522df35ace
Seth-Gbemumu <gracesefor@gmail.com>
2024-07-07 09:41:31 -0700

    Temperature coverter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,11 +2,11 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    return FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    return (fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR) - 32
 #convert_to_celsius(fahrenheit)
 
 def convert_to_fahrenheit(celsius):
-    return CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 #convert_to_celsius(celsius)
 
 def main():
