83d88f621fab9c9f6a2b7c62cfefc5b30473910d
Seth-Gbemumu <gracesefor@gmail.com>
2024-07-21 04:22:55 -0700

    Create library system for books

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,11 +7,13 @@ class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}."
     
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+        return f"PrintBook: {self.title} by {self.author}, File Size: {self.page_count}"
 
 class Library:
     def __init__(self):
@@ -22,9 +24,4 @@ class Library:
     
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}.")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.author} by {book.author}, Page Count: {book.page_count}.")
-            else:
-                print(f"{book.title} by {book.author}")
+            print(book)
